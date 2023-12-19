491b98896254fbb9c9c6991f6a2b3b70384e2d22
Andydandy21 <a.smithb21@gmail.com>
2023-12-19 17:45:32 -0600

    Initial commit

---
+++ test/ssr_sandbox.test.ts
@@ -0,0 +1,17 @@
+// import * as cdk from 'aws-cdk-lib';
+// import { Template } from 'aws-cdk-lib/assertions';
+// import * as SsrSandbox from '../lib/ssr_sandbox-stack';
+
+// example test. To run these tests, uncomment this file along with the
+// example resource in lib/ssr_sandbox-stack.ts
+test('SQS Queue Created', () => {
+//   const app = new cdk.App();
+//     // WHEN
+//   const stack = new SsrSandbox.SsrSandboxStack(app, 'MyTestStack');
+//     // THEN
+//   const template = Template.fromStack(stack);
+
+//   template.hasResourceProperties('AWS::SQS::Queue', {
+//     VisibilityTimeout: 300
+//   });
+});
