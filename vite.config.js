3f3ce12ae0a3dc27b4ed71a0d460f959a94f3591
Andydandy21 <a.smithb21@gmail.com>
2023-12-21 13:58:15 -0600

    include vite in repository

---
+++ vite.config.js
@@ -0,0 +1,17 @@
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  plugins: [react()],
+  build: {
+    rollupOptions: {
+      input: {
+        index: "/src/entry-client.tsx",
+      },
+      output: {
+        entryFileNames: "[name].js",
+      },
+    },
+  },
+});
